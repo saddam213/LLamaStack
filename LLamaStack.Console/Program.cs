e0d6b3280e2af3dfaaf42e8e0baa7497c4a39cef
sa_ddam213 <sa_ddam213@live.com>
2023-09-03 16:03:18 +1200

    Update Console app, add README

--- LLamaStack.Console/Program.cs
+++ LLamaStack.Console/Program.cs
@@ -1,53 +1,39 @@
 ﻿using LLamaStack.Core;
-using LLamaStack.Core.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace LLamaStack.Console
 {
     internal class Program
     {
-        private static IHost _applicationHost;
-
         static async Task Main(string[] args)
         {
             var builder = Host.CreateApplicationBuilder();
+            builder.Logging.ClearProviders();
 
             // Add LLamaStack
-            builder.Services.AddLogging((loggingBuilder) => loggingBuilder.SetMinimumLevel(LogLevel.Trace));
+            builder.Services.AddLogging((loggingBuilder) => loggingBuilder.SetMinimumLevel(LogLevel.Error));
             builder.Services.AddLLamaStack<string>();
 
-            // Add Services
+            // Add AppService
             builder.Services.AddHostedService<AppService>();
-            builder.Services.AddTransient<App>();
-            builder.Services.AddTransient<AppTest>();
 
-            _applicationHost = builder.Build();
-            await _applicationHost.RunAsync();
-        }
-    }
+            // Add Runners
+            var exampleRunners = Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(type => typeof(IExampleRunner).IsAssignableFrom(type) && !type.IsInterface)
+                .ToList();
+            builder.Services.AddSingleton(exampleRunners.AsEnumerable());
+            foreach (var exampleRunner in exampleRunners)
+            {
+                builder.Services.AddSingleton(exampleRunner);
+            }
 
-    internal class AppService : IHostedService
-    {
-        private App _app;
-        private AppTest _appTest;
-
-        public AppService(App app, AppTest appTest)
-        {
-            _app = app;
-            _appTest = appTest;
+            // Start
+            await builder.Build().RunAsync();
         }
 
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            await _app.RunAsync();
-            // await _appTest.RunAsync();
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            return Task.CompletedTask;
-        }
     }
 }
